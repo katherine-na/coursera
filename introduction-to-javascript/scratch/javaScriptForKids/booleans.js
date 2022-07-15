cb33da18139fc07200a0b8b4820f4430f85e19ee
Katherine Negrete <katherine.negrete.aguilar@gmail.com>
2022-07-15 15:42:19 -0500

    chore(javascript): new javascript files
    Create new javascript files with examples about string, boolean,
    number and array.

---
+++ introduction-to-javascript/scratch/javaScriptForKids/booleans.js
@@ -0,0 +1,21 @@
+var hadShower = true;
+var hasBackpack = false;
+console.log(hadShower && hasBackpack);
+
+var hasApple = true;
+var hasOrange = false;
+console.log(hasApple || hasOrange);
+
+var isWeekend = true;
+var needToShowerToday = !isWeekend;
+console.log(needToShowerToday);
+
+// comparing numbers with booleans
+
+var height = 65;
+var heightRestriccion = 60;
+console.log(height > heightRestriccion);
+
+var age = 12;
+var accompanied = true;
+console.log();
