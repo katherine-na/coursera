bdd85279750641d0afb1a336bce7dc637a31995d
Katherine Negrete <katherine.negrete.aguilar@gmail.com>
2022-07-26 11:54:51 -0500

    chore(content): clean files

--- introduction-to-javascript/scratch/playground/collecMain.js
+++
@@ -1,9 +0,0 @@
-function upDate(previewPic) {
-  const displayPic = document.getElementById("display-container");
-  displayPic.style.backgroundImage = `url(${previewPic.src})`;
-}
-
-function unDo() {
-  const unDoPic = document.getElementById("display-container");
-  unDoPic.style.backgroundImage = `url(${""})`;
-}
